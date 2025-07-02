ab2b8398d4c9be18ff34ddd3136a2f3eaa4aa17c
lolzob <101523800+lolzob@users.noreply.github.com>
2025-07-02 13:29:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <h1>ChampQuest</h1>
+        <p>Intră în pielea unui manager de fotbal. Creează-ți echipa. Condu-o spre glorie!</p>
       </header>
+      <main>
+        <button>Înscrie-te</button>
+        <button>Autentificare</button>
+      </main>
     </div>
   );
 }
